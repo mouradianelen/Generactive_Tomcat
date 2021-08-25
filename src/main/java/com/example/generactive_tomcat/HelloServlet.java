3ecf8d2c7f432103ca65708683b97cc643ce444d
mouradianelen <elen_muradyan@edu.aua.am>
2021-08-25 20:09:36 +0400

    2nd commit

--- src/main/java/com/example/generactive_tomcat/HelloServlet.java
+++ src/main/java/com/example/generactive_tomcat/HelloServlet.java
@@ -8,24 +8,24 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.*;
 
 
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
+//@WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
-    private String message;
-
-    public void init() {
-        message = "Hello World!";
-    }
-
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
-
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
-    }
-
-    public void destroy() {
-    }
+//    private String message;
+//
+//    public void init() {
+//        message = "Hello World!";
+//    }
+//
+//    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+//        response.setContentType("text/html");
+//
+//        // Hello
+//        PrintWriter out = response.getWriter();
+//        out.println("<html><body>");
+//        out.println("<h1>" + message + "</h1>");
+//        out.println("</body></html>");
+//    }
+//
+//    public void destroy() {
+//    }
 }
--- src/main/java/servlets/itemsServlet.java
+++ src/main/java/servlets/itemsServlet.java
@@ -15,17 +15,20 @@ import servlets.util.HttpConstants;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.stream.Collectors;
 
 
-@WebServlet("/items")
+@WebServlet(name = "itemsServlet", value = "/items")
 
 public class itemsServlet extends HttpServlet {
+    private final ItemRepository items = ItemRepository.getInstance();
+
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws
             ServletException, IOException {
         // ObjectMapper parse Java objects to JSON and vise a versa.
-        ItemRepository items=ItemRepository.getInstance();
+//        ItemRepository items=ItemRepository.getInstance();
         ObjectMapper objectMapper = new ObjectMapper();
         // accept only application/json
         if (!req.getContentType().equals(HttpConstants.ContentType.APPLICATION_JSON)) {
@@ -39,11 +42,34 @@ public class itemsServlet extends HttpServlet {
         try {
             StockItem item = objectMapper.readValue(body, StockItem.class);
             items.addItem(item);
-            resp.getWriter().write(objectMapper.writeValueAsString(item));
+
         } catch (RuntimeException e) {
             ErrorEntity errorEntity = new ErrorEntity("json_parse_failed:" + e.getMessage());
             resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            resp.getWriter().write(objectMapper.writeValueAsString(errorEntity));
+
         }
+        System.out.printf("mmckkdnd");
+
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println(req.getContextPath());
+        System.out.println(req.getMethod());
+        System.out.println(req.getPathInfo());
+        System.out.println(req.getPathTranslated());
+        System.out.println(req.getRequestURI());
+        System.out.println(req.getServletPath());
+        System.out.println(req.getRemoteAddr());
+        System.out.println(req.getRemoteHost());
+        System.out.println(req.getQueryString());
+        System.out.println(req.getServerName());
+        resp.setContentType(HttpConstants.ContentType.APPLICATION_JSON);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String response = objectMapper.writeValueAsString(items);
+        PrintWriter writer = resp.getWriter();
+        writer.write(response);
     }
 }
